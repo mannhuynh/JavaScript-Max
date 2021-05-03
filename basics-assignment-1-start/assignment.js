d67fe4bdda6a60123b86bd462109d2c17e08cadb
Vuong Huynh <mannhuynh@gmail.com>
2021-05-03 09:45:21 -0500

    First assignment

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,17 @@
+const userInput = 20;
+
+
+let addResult = 18 + userInput;
+
+let subtrResult = addResult - userInput;
+
+let multiResult = addResult * 2;
+
+let divResult = multiResult / 4;
+
+alert("User input: " + userInput)
+alert("Addition: " + addResult)
+alert("Subtraction: " + subtrResult)
+alert("Multiplication: " + multiResult)
+alert("Division: " + divResult)
+
